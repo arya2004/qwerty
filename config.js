755b05380a3ebf32553411c7c53eaf3dfad66d86
arya2004 <arya20j4@gmail.com>
2023-12-09 03:57:25 +0530

    a

--- config.js
+++ config.js
@@ -2,3 +2,5 @@
   export const contractAddress = "0x67707460E2498534DDDBEDc228Fc523F2eF6C1A4"
   export const ownerAddress = "0x254B2a8373baf7B8dC61E15891434241e4D16736"
   
+ // graph init --from-contract 0x67707460E2498534DDDBEDc228Fc523F2eF6C1A4  --network mumbai --contract-name Blog --index-events
+ // artifacts/contracts/Blog.sol/Blog.json
